8420281f033a72cb6a96f4b0a2055165c5fe814b
Aaron Weissman <weissman.aaron@gmail.com>
2019-10-06 16:09:26 -0600

    Add hikes route and template.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hike = require('./routes/hike');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,18 +18,21 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/hikes', hike.index);
+app.post('/add_hike', hike.add_hike);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
